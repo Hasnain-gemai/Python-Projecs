2bf41d460b8d9db0404402591149eb7d0313d2eb
Hasnain Yousaf <hasnaingemai@gmail.com>
2025-09-29 23:47:23 +0500

    Add Code to make it more Efficient

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "sky": "https://youtu.be/b98IFRzfqcA?si=CuVKPARCL5s-juxL",
+    "land": "https://youtu.be/9QkYc0x9BE8?si=hlixOGK4TcTjjtf2",
+    "mountain": "https://youtu.be/BSJa1UytM8w?si=9bSK5kD3MDd1E9hb",
+    "millionaire": "https://youtu.be/XO8wew38VM8?si=NOWr4qKfTF-c54FZ"
+}
